308e04a06512a3c68a69b15dd18c45ebdd998588
Sneaxiii <misterbalise2@gmail.com>
2026-02-19 21:10:27 +0100

    Adds MainMawsterLogo component with logo image and remove titap

--- front/app/ui/MawsterLogo.tsx
+++ front/app/ui/MawsterLogo.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 
-interface MainCesiZenLogoProps {
+interface MainMawsterLogoProps {
   className?: string;
 }
 
-export default function MainCesiZenLogo({ className = '' }: MainCesiZenLogoProps) {
+export default function MainMawsterLogo({ className = '' }: MainMawsterLogoProps) {
   return (
     <div
       className={`flex items-center gap-4 leading-none text-white ${className}`}
-      aria-label='Cesi Zen Logo'
+      aria-label='Mawster Logo'
     >
       <Image
         src='/logos/main_logo.png'
-        alt='Logo Cesi Zen'
+        alt='Logo Mawster'
         width={48}
         height={48}
       />
-      <p className='text-xl font-semibold md:text-2xl'>Cesi Zen</p>
+      <p className='text-xl font-semibold md:text-2xl'>Mawster</p>
     </div>
   );
 }
