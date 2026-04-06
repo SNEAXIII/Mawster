f228d7ccc53440ca4d1e46bf9f9dc54acd2dfd05
Sneaxiii <misterbalise2@gmail.com>
2026-04-06 03:31:58 +0200

    refactor: enhance PrefightBadge and SynergyBadge components with additionalClasses prop, update ChampionPortrait for improved class handling

--- front/app/game/war/_components/synergy-badge.tsx
+++ front/app/game/war/_components/synergy-badge.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import { cn } from '@/app/lib/utils';
 import { Zap } from 'lucide-react';
 
-export default function SynergyBadge() {
-  return <Zap className='w-2.5 h-2.5' />;
+export default function SynergyBadge({additionalClasses = ''}: Readonly<{additionalClasses?: string}>) {
+  return (
+    <div
+      className={cn(
+        'absolute bottom-0 right-0 bg-primary text-primary-foreground rounded-full p-0.5 flex items-center justify-center',
+        additionalClasses
+      )}
+    >
+      <Zap className='w-2.5 h-2.5' />
+    </div>
+  );
 }
