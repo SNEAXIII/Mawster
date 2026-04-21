431a3ab31f6b378470851c8aae2745eba9925f57
Sneaxiii <misterbalise2@gmail.com>
2026-04-21 12:22:06 +0000

    feat: update front run.sh to read Docker secrets from /run/secrets/

--- front/run.sh
+++ front/run.sh
@@ -1,3 +1,8 @@
 set -e
 /bin/bash ./wait-for-it.sh api:8000 -t 60 --strict
+
+export NEXTAUTH_SECRET=$(cat /run/secrets/mawster_nextauth_secret)
+export DISCORD_CLIENT_ID=$(cat /run/secrets/mawster_discord_client_id)
+export DISCORD_CLIENT_SECRET=$(cat /run/secrets/mawster_discord_client_secret)
+
 node server.js
