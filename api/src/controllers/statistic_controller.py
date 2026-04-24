a519f2213c4d68af767b6cb2095ebce1fc3b72ae
Sneaxiii <misterbalise2@gmail.com>
2026-04-24 19:56:01 +0000

    wip

--- api/src/controllers/statistic_controller.py
+++ api/src/controllers/statistic_controller.py
@@ -1,5 +1,7 @@
+import uuid
 from typing import Annotated
-from src.models import GameAccount
+from src.models import ChampionUser, Season, War, WarDefensePlacement, GameAccount
+from sqlalchemy import func
 from sqlmodel import select
 
 from fastapi import APIRouter, Depends
@@ -12,8 +14,8 @@ statistics_controller = APIRouter(
     prefix="/statistics",
     tags=["Statistics"],
     dependencies=[
-        Depends(AuthService.is_logged_as_user),
-        Depends(AuthService.get_current_user_in_jwt),
+        # Depends(AuthService.is_logged_as_user),
+        # Depends(AuthService.get_current_user_in_jwt),
     ],
 )
 
@@ -24,12 +26,28 @@ statistics_controller = APIRouter(
 )
 async def get_current_season_statistics(
     session: SessionDep,
-    current_user: Annotated[User, Depends(AuthService.get_current_user_in_jwt)],
+    # current_user: Annotated[User, Depends(AuthService.get_current_user_in_jwt)],
 ):
     """Get the current season statistics."""
-    sql = select(GameAccount.id, GameAccount.game_pseudo, GameAccount.alliance_group).where(
-        GameAccount.alliance_id == current_user.alliance_id
+    sql = (
+        select(
+            GameAccount.id,
+            GameAccount.game_pseudo,
+            GameAccount.alliance_group,
+            func.count().label("total_fights"),
+        )
+        .join(ChampionUser, ChampionUser.game_account_id == GameAccount.id)
+        .join(WarDefensePlacement, WarDefensePlacement.attacker_champion_user_id == ChampionUser.id)
+        .join(War, WarDefensePlacement.war_id == War.id)
+        .join(Season, War.season_id == Season.id)
+        .where(
+            GameAccount.alliance_id == uuid.UUID("7e7b2e7c-7a51-4b14-95b8-fe21558a0146")
+            # GameAccount.alliance_id == current_user.alliance_id
+        )
+        .group_by(GameAccount.id, GameAccount.game_pseudo, GameAccount.alliance_group)
     )
     session_result = (await session.exec(sql)).all()
-    print(session_result)
+    for elem in session_result:
+        print(elem)
     print(5)
+    return None
