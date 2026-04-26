83b444b9b1dd660a4cd69babaa1b817f3dc30fd7
Sneaxiii <misterbalise2@gmail.com>
2026-04-26 02:06:20 +0200

    refactor: simplify setup for attackers count badge tests

--- front/cypress/e2e/war/attackers-count.cy.ts
+++ front/cypress/e2e/war/attackers-count.cy.ts
@@ -6,21 +6,13 @@ describe('War – attackers count badge color', () => {
   });
 
   it('count badge is red when fewer than 50 attackers are assigned', () => {
-    setupAttackerScenario('war-cnt-red').then(
-      ({ adminToken, ownerData, allianceId, ownerAccId, warId }) => {
-        cy.apiLoadChampion(adminToken, 'Iron Man', 'Tech').then((champs) => {
-          cy.apiAddChampionToRoster(ownerData.access_token, ownerAccId, champs[0].id, '7r3').then((cu) => {
-            cy.apiAssignWarAttacker(ownerData.access_token, allianceId, warId, 1, 1, cu.id);
-          });
-        });
+    setupAttackerScenario('war-cnt-red').then(({ ownerData }) => {
+      cy.apiLogin(ownerData.user_id);
+      cy.navTo('war');
 
-        cy.apiLogin(ownerData.user_id);
-        cy.navTo('war');
-
-        cy.getByCy('attackers-count').should('have.class', 'text-red-400');
-        cy.getByCy('attackers-count').should('not.have.class', 'text-yellow-400');
-      },
-    );
+      cy.getByCy('attackers-count').should('have.class', 'text-red-400');
+      cy.getByCy('attackers-count').should('not.have.class', 'text-yellow-400');
+    });
   });
 
   it('count badge is gold when all 50 attackers are assigned', () => {
