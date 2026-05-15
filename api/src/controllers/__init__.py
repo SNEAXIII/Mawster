d66549297fb59c1daa5785f4e67eccf0d8c71f90
Sneaxiii <misterbalise2@gmail.com>
2026-05-15 02:48:03 +0200

    Move controleurs

--- api/src/controllers/__init__.py
+++ api/src/controllers/__init__.py
@@ -1,15 +1,18 @@
-from src.controllers.admin_controller import admin_controller
-from src.controllers.auth_controller import auth_controller
-from src.controllers.user_controller import user_controller
-from src.controllers.game_account_controller import game_account_controller
-from src.controllers.alliance_controller import alliance_controller
-from src.controllers.champion_user_controller import champion_user_controller
-from src.controllers.champion_controller import champion_controller, champion_read_controller
-from src.controllers.defense_controller import defense_controller
-from src.controllers.war_controller import war_controller
-from src.controllers.season_controller import season_admin_controller, season_public_controller
-from src.controllers.statistic_controller import statistics_controller
-from src.controllers.fight_record_controller import fight_record_controller
+from src.controllers.admin.admin_controller import admin_controller
+from src.controllers.admin.champion_controller import champion_controller, champion_read_controller
+from src.controllers.admin.season_controller import (
+    season_admin_controller,
+    season_public_controller,
+)
+from src.controllers.admin.fight_record_controller import fight_record_controller
+from src.controllers.auth.auth_controller import auth_controller
+from src.controllers.account.user_controller import user_controller
+from src.controllers.account.game.game_account_controller import game_account_controller
+from src.controllers.account.game.champion_user_controller import champion_user_controller
+from src.controllers.alliance.alliance_controller import alliance_controller
+from src.controllers.alliance.war.defense_controller import defense_controller
+from src.controllers.alliance.war.war_controller import war_controller
+from src.controllers.alliance.war.statistic_controller import statistics_controller
 
 routers = [
     admin_controller,
