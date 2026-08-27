be860f0da20911a882a82640aeb9bdcc85b0b282
Sneaxiii <misterbalise2@gmail.com>
2026-08-27 09:33:18 +0000

    shelldre:S7688

--- api/migrate.sh
+++ api/migrate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
-if [ -f /run/secrets/mawster_db_password ]; then
+if [[ -f /run/secrets/mawster_db_password ]]; then
   export MARIADB_PASSWORD=$(cat /run/secrets/mawster_db_password)
 fi
-if [ -f /run/secrets/mawster_db_root_password ]; then
+if [[ -f /run/secrets/mawster_db_root_password ]]; then
   export MARIADB_ROOT_PASSWORD=$(cat /run/secrets/mawster_db_root_password)
 fi
 
