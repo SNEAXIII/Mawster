6637960681ae45d29ce32ef74c22159bf2d7c354
Sneaxiii <misterbalise2@gmail.com>
2026-08-28 08:41:06 +0000

    refactor for sonar

--- api/run.sh
+++ api/run.sh
@@ -10,7 +10,7 @@ export EMAIL_PEPPER="${EMAIL_PEPPER:-$(cat /run/secrets/mawster_email_pepper)}"
 # won't boot without them. A deployment without RustFS nor RabbitMQ sets
 # VISION_ENABLED=0: those secrets do not exist there, and `cat` would only
 # export empty strings while printing errors.
-if [ "${VISION_ENABLED:-1}" != "0" ]; then
+if [[ "${VISION_ENABLED:-1}" != "0" ]]; then
   export RABBITMQ_URL="${RABBITMQ_URL:-amqp://mawster:$(cat /run/secrets/mawster_rabbitmq_pass)@rabbitmq:5672/}"
   export RUSTFS_ACCESS_KEY="${RUSTFS_ACCESS_KEY:-$(cat /run/secrets/mawster_rustfs_access_key)}"
   export RUSTFS_SECRET_KEY="${RUSTFS_SECRET_KEY:-$(cat /run/secrets/mawster_rustfs_secret_key)}"
