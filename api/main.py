2f5776a2c5f84705328652891dffccdb05dc5906
SNEAXIII <74428850+SNEAXIII@users.noreply.github.com>
2026-09-09 18:33:50 +0200

    chore(main): release 1.13.0

--- api/main.py
+++ api/main.py
@@ -64,7 +64,7 @@ async def lifespan(_: FastAPI):
     await consumer.stop()
 
 
-app = FastAPI(title="Mawster", version="1.12.1", lifespan=lifespan)  # x-release-please-version
+app = FastAPI(title="Mawster", version="1.13.0", lifespan=lifespan)  # x-release-please-version
 Instrumentator().instrument(app)
 
 # Rate limiter (utilise l'IP du client — X-Forwarded-For si disponible, sinon connexion directe)
